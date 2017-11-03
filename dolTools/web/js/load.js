1db1b833d14341c263d0d078fe615b2b96fee49f
Roy <roy@iresearch.com.cn>
2017-11-03 14:48:30 +0800

    load.js

---
+++ dolTools/web/js/load.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var url = '../html/main.html';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send(null);
+    var result = xmlhttp.status;
+    if (result == 200) {
+        document.write(xmlhttp.responseText);
+    }
+})();
